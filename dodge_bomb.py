946c503a9f4c78893d0c5933001dad2eb5dbb969
sugimoto <c0b22079b2@edu.teu.ac.jp>
2023-11-28 16:40:58 +0900

    演習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -33,6 +33,16 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
+    kk_lst = {(0,0): pg.transform.flip(kk_img,True, False)}
+    kk_lst[(0,5)]= pg.transform.flip(pg.transform.rotate(kk_img,90),True, False)
+    kk_lst[(5,5)]= pg.transform.flip(pg.transform.rotate(kk_img,45),True, False)
+    kk_lst[(5,0)]= pg.transform.flip(pg.transform.rotate(kk_img,0),True, False)
+    kk_lst[(5,-5)]= pg.transform.flip(pg.transform.rotate(kk_img,-45),True, False)
+    kk_lst[(0,-5)]= pg.transform.flip(pg.transform.rotate(kk_img,-90),True, False)
+    kk_lst[(-5,-5)]=(pg.transform.rotate(kk_img,-45))
+    kk_lst[(-5,0)]=(pg.transform.rotate(kk_img,0))
+    kk_lst[(-5,5)]=(pg.transform.rotate(kk_img,45))
+    accs = [a for a in range(1, 11)]
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rct = kk_img.get_rect()  # 練習３：こうかとんSurfaceのRectを抽出する
     kk_rct.center = 900, 400  # 練習３：こうかとんの初期座標
@@ -42,10 +52,13 @@ def main():
     bb_rct = bb_img.get_rect()  # 練習１：爆弾SurfaceのRectを抽出する
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
-    vx, vy = +5, +5  # 練習２：爆弾の速度
-
+    time=0
+    time2=1
+    vx, vy = 0,0  # 練習２：爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
+    vxx=1
+    vyy=1
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -62,25 +75,35 @@ def main():
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
         
-
+        
         screen.blit(bg_img, [0, 0])
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
         if check_bound(kk_rct) != (True, True):
-            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        screen.blit(kk_img, kk_rct)  # 練習３：こうかとんを移動させる
-        bb_rct.move_ip(vx, vy)  # 練習２：爆弾を移動させる
+            
+                kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(kk_lst[tuple(sum_mv)], kk_rct)  # 練習３：こうかとんを移動させる
+        if time2 <10:
+            vx= (time2)*vxx
+            vy= (time2)*vyy
+        elif time2>=10:
+            vx=10*vxx
+            vy=10*vyy
         yoko, tate = check_bound(bb_rct)
         if not yoko:  # 横方向にはみ出たら
-            vx *= -1
+            vx*= -1
+            vxx*=-1
         if not tate:  # 縦方向にはみ出たら
             vy *= -1
-        bb_rct.move_ip(vx, vy) 
+            vyy*=-1
+        bb_rct.move_ip(vx,vy) 
         screen.blit(bb_img, bb_rct)
         pg.display.update()
+        print(vx)
         tmr += 1
-        clock.tick(50)
-
-
+        clock.tick(100)
+        time+=1
+        if time%100==99:
+            time2+=1
 if __name__ == "__main__":
     pg.init()
     main()
